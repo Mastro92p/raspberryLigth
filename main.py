481bde8e2754a51c34b1cd1a9e2a2644590c0196
mastro92p <mastro92p@gmail.com>
2019-10-30 19:42:51 +0100

    initial test

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import sys, os
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4,GPIO.IN)
+
 
 def main():
-    print('hello world')
+    while True:
+        print(GPIO.input(4))
 
 if __name__ == '__main__':
     main()
